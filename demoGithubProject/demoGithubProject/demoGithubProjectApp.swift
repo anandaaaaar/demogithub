36c208a07ccafe02e99e68af0557d0d59ec1fc7e
AtxZmXe <azmeemitnoy@gmail.com>
2024-06-22 20:04:01 -0700

    firstcommic

---
+++ demoGithubProject/demoGithubProject/demoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoGithubProjectApp.swift
+//  demoGithubProject
+//
+//  Created by Admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1aafd7b429f5820eed573a2a938756d4b8efab88
AtxZmXe <azmeemitnoy@gmail.com>
2024-06-22 20:58:25 -0700

    dwdwd

--- demoGithubProject/demoGithubProject/ContentView.swift
+++ demoGithubProject/demoGithubProject/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Spacer()
+            Image(systemName: "person.crop.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Github")
             Text("Hey!")
+        https://www.figma.com/design/Lpak3YECUTCIh6fTJ2VBV5/Untitled?node-id=0-1&t=zlEjGQYzUtkkW9DZ-1
             
         }
         .padding()

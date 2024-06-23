4c3f3d9af7b5de8880a99e49b87ca701c74a5afe
AtxZmXe <azmeemitnoy@gmail.com>
2024-06-22 20:26:20 -0700

    o3j2o

--- demoGithubProject/demoGithubProject/ContentView.swift
+++ demoGithubProject/demoGithubProject/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Github")
+            Text("Hey!")
             
         }
         .padding()
